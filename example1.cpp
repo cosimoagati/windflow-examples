df4b7d96de1eee6eb34fdc91c45226640b360fc9
cosimone64 <cosimo.agati@disroot.org>
2021-11-13 23:12:32 +0100

    Add basic description of the program in a comment.

--- example1.cpp
+++ example1.cpp
@@ -1,3 +1,8 @@
+/*
+ * The dataset is formed by the strings passed as input to the program.  Run as
+ * ./example1 <string1> <string2> ...
+ */
+
 #include <iostream>
 #include <string>
 #include <unordered_map>
