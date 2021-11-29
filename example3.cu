8b5edaaa123cc1d30de82b4b0e594e737b21dee2
cosimone64 <cosimo.agati@disroot.org>
2021-11-29 10:44:13 +0100

    Remove warning, fixed upstream.

--- example3.cu
+++ example3.cu
@@ -2,9 +2,6 @@
  * The dataset is formed by the strings passed as input to the program.  Run as
  * ./example1 <string1> <string2> ...
  */
-#include <chrono> // TODO: This has to be included because windflow_gpu.hpp uses
-                  // chrono without including the header!  This must be
-                  // patched...
 #include <cstdlib>
 #include <iostream>
 #include <string>
