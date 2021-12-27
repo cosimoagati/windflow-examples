b3d9fd20f34fe47624900c1874695e9f6fbfff85
cosimone64 <cosimo.agati@disroot.org>
2021-12-27 20:21:37 +0100

    Add basic pipeline construction.

--- sa-sentiment-analysis.cpp
+++ sa-sentiment-analysis.cpp
@@ -106,5 +106,35 @@ static void do_sink(optional<pair<string, SentimentResult>> &input) {
 
 int main(int argc, char *argv[]) {
     cout << "TODO: initalize pipeline...\n";
+    const auto           use_chaining = false;
+    const vector<string> dataset;
+
+    SourceFunctor source_functor {dataset};
+    auto          source = Source_Builder(source_functor)
+                      .withParallelism(1)
+                      .withName("source")
+                      .build();
+
+    MapFunctor<BasicClassifier> map_functor;
+    auto                        classifier_node =
+        Map_Builder(map_functor)
+            .withParallelism(1)
+            .withName("counter")
+            .withKeyBy(
+                [](const std::string &word) -> std::string { return word; })
+            .build();
+
+    auto sink =
+        Sink_Builder(do_sink).withParallelism(1).withName("sink").build();
+
+    PipeGraph graph {"filtered_wc", Execution_Mode_t::DEFAULT,
+                     Time_Policy_t::INGRESS_TIME};
+    if (use_chaining) {
+        graph.add_source(source).add(classifier_node).chain_sink(sink);
+    } else {
+        graph.add_source(source).add(classifier_node).add_sink(sink);
+    }
+    graph.run();
+
     return 0;
 }
