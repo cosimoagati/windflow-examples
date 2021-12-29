8024288ced86b9bef9b4a5665a613d10a8092a0d
cosimone64 <cosimo.agati@disroot.org>
2021-12-29 15:32:57 +0100

    Change milliseconds to seconds.

--- sa-sentiment-analysis/sa.cpp
+++ sa-sentiment-analysis/sa.cpp
@@ -180,9 +180,9 @@ static inline void parse_and_validate_args(int argc, char **argv,
 }
 
 int main(int argc, char *argv[]) {
-    const auto   start_time   = steady_clock::now();
-    const auto   use_chaining = false;
-    milliseconds duration {0};
+    const auto start_time   = steady_clock::now();
+    const auto use_chaining = false;
+    seconds    duration {0};
 
     parse_and_validate_args(argc, argv, duration);
 
