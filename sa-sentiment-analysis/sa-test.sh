ce204e578d9220e78dfbf603149766fe601fb14d
cosimone64 <cosimo.agati@disroot.org>
2021-12-31 12:25:13 +0100

    Remove test output file if it already exists.

--- sa-sentiment-analysis/sa-test.sh
+++ sa-sentiment-analysis/sa-test.sh
@@ -3,6 +3,10 @@
 output_file=sa-test-output.txt
 duration=120
 
+if [ -f "$output_file" ]; then
+    rm "$output_file"
+fi
+
 for chaining in false true; do
     for i in $(seq 1 28); do
 	echo "Results with -t $duration -m $i -c $chaining" >> "$output_file"
