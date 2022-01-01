3e865b999b79c0746d33947d4aada2af6ea47d51
cosimone64 <cosimo.agati@disroot.org>
2022-01-01 16:46:19 +0100

    Output test date before starting.

--- sa-sentiment-analysis/sa-test.sh
+++ sa-sentiment-analysis/sa-test.sh
@@ -7,6 +7,8 @@ if [ -f "$output_file" ]; then
     rm "$output_file"
 fi
 
+echo "Test performed on $(date)" >> "$output_file"
+
 for chaining in false true; do
     for i in $(seq 1 28); do
 	echo "Results with -t $duration -m $i -c $chaining" >> "$output_file"
