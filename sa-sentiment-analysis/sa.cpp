dc5575a722068e6f55bd064c993d73d8acac4886
cosimone64 <cosimo.agati@disroot.org>
2022-01-05 12:30:21 +0100

    Use the correct hashes in CachingClassifier.
    For the result cache, we can map directly using strings as keys, since
    we don’t need "compatibility" with string views.  Therefore, we just
    use an (unordered_)map<string, SentimentResult> for the result cache,
    and hash the tweet directly to provide potential cached results.  Of
    course, if there is no cached result, we need to compute it on the
    fly, as BasicClassifier always does.

--- sa-sentiment-analysis/sa.cpp
+++ sa-sentiment-analysis/sa.cpp
@@ -350,29 +350,29 @@ public:
 };
 
 class CachingClassifier {
-    static constexpr auto             default_path = "AFINN-111.txt";
-    hash<string_view>                 gethash;
-    unordered_map<unsigned long, int> sentiment_map;
-    unordered_map<unsigned long, SentimentResult> result_cache;
+    static constexpr auto                  default_path = "AFINN-111.txt";
+    hash<string_view>                      gethash;
+    unordered_map<unsigned long, int>      sentiment_map;
+    unordered_map<string, SentimentResult> result_cache;
 
 public:
     CachingClassifier(const char *path = default_path)
         : sentiment_map {get_sentiment_map<decltype(sentiment_map)>(path)} {}
 
     SentimentResult classify(string &tweet) {
-        const auto tweet_hash    = gethash(tweet);
-        const auto cached_result = result_cache.find(tweet_hash);
+        const auto cached_result = result_cache.find(tweet);
 
         if (cached_result != result_cache.end()) {
             return cached_result->second;
         }
-        auto &     result_cache_entry      = result_cache[tweet_hash];
+        auto &     result_cache_entry      = result_cache[tweet];
         const auto words                   = split_in_words_in_place(tweet);
         auto       current_tweet_sentiment = 0;
 
         for (const auto &word : words) {
-            if (sentiment_map.find(tweet_hash) != sentiment_map.end()) {
-                current_tweet_sentiment += sentiment_map[tweet_hash];
+            const auto word_hash = gethash(word);
+            if (sentiment_map.find(word_hash) != sentiment_map.end()) {
+                current_tweet_sentiment += sentiment_map[word_hash];
             }
         }
         result_cache_entry = {score_to_sentiment(current_tweet_sentiment),
