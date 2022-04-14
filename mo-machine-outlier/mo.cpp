70c4d7727cea47c1a672470febff1186923a69c9
cosimone64 <cosimo.agati@disroot.org>
2022-04-14 22:14:36 +0200

    Add assert for index

--- mo-machine-outlier/mo.cpp
+++ mo-machine-outlier/mo.cpp
@@ -872,6 +872,7 @@ public:
                 const size_t median_idx {stream_list.size() / 2};
                 const auto   min_score = abnormal_streams[0].anomaly_score;
                 const auto   median_score =
+                assert(median_idx < abnormal_streams.size());
                     abnormal_streams[median_idx].anomaly_score;
 
                 for (size_t i {0}; i < abnormal_streams.size(); ++i) {
