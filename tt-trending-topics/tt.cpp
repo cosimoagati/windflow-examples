4bd0483b38507d3bef280c228798b921c2092319
cosimone64 <cosimo.agati@disroot.org>
2022-04-18 15:31:03 +0200

    Add TopicExtractorFunctor and reorder code (lazy commit)

--- tt-trending-topics/tt.cpp
+++ tt-trending-topics/tt.cpp
@@ -228,6 +228,207 @@ public:
         return metric_name.c_str();
     }
 };
+template<typename T>
+class SlotBasedCounter {
+    unordered_map<T, vector<unsigned long>> counts_map;
+    size_t                                  num_slots;
+
+    unsigned long compute_total_count(T obj) {
+        if (counts_map.find(obj) == counts_map.end()) {
+            return 0;
+        }
+
+        const auto &  curr_obj_counts = counts_map[obj];
+        unsigned long total {0};
+        for (const auto count : curr_obj_counts) {
+            total += count;
+        }
+        return total;
+    }
+
+    void reset_slot_count_to_zero(T obj, unsigned slot) {
+        counts_map[obj][slot] = 0;
+    }
+
+    bool should_be_removed_from_counter(T obj) {
+        return compute_total_count(obj) == 0;
+    }
+
+public:
+    SlotBasedCounter(size_t num_slots) : num_slots {num_slots} {}
+
+    void increment_count(T obj, size_t slot, unsigned long increment) {
+        assert(slot < num_slots);
+
+        // XXX: this should work as expected, but double check!
+        if (counts_map.find(obj) == counts_map.end()) {
+            counts_map[obj].reserve(num_slots);
+        }
+        counts_map[obj][slot] += increment;
+    }
+
+    void increment_count(T obj, size_t slot) {
+        increment_count(obj, slot, 1);
+    }
+
+    unsigned long get_count(T obj, size_t slot) {
+        assert(slot < num_slots);
+
+        if (counts_map.find(obj) == counts_map.end()) {
+            return 0;
+        }
+        return counts_map[obj][slot];
+    }
+
+    unordered_map<T, unsigned long> get_counts() {
+        unordered_map<T, unsigned long> result;
+
+        for (const auto &kv : counts_map) {
+            result[kv.first] = compute_total_count(kv.first);
+        }
+        return result;
+    }
+
+    void wipe_slot(size_t slot) {
+        assert(slot < num_slots);
+
+        for (const auto &kv : counts_map) {
+            reset_slot_count_to_zero(kv.first, slot);
+        }
+    }
+
+    void wipe_zeros() {
+        vector<T> objs_to_be_removed;
+        for (const auto &kv : counts_map) {
+            if (should_be_removed_from_counter(kv.first)) {
+                objs_to_be_removed.push_back(kv.first);
+            }
+        }
+        for (const auto &obj : objs_to_be_removed) {
+            counts_map.erase(obj);
+        }
+    }
+};
+
+template<typename T>
+class SlidingWindowCounter {
+    SlotBasedCounter<T> obj_counter;
+    size_t              head_slot;
+    size_t              tail_slot;
+    size_t              windw_length_in_slots;
+
+    size_t slot_after(size_t slot) {
+        assert(slot < windw_length_in_slots);
+        return (slot + 1) % windw_length_in_slots;
+    }
+
+    void advance_head() {
+        head_slot = tail_slot;
+        tail_slot = slot_after(tail_slot);
+    }
+
+public:
+    SlidingWindowCounter(size_t windw_length_in_slots)
+        : obj_counter {windw_length_in_slots}, head_slot {0},
+          tail_slot {slot_after(head_slot)}, windw_length_in_slots {
+                                                 windw_length_in_slots} {
+        if (windw_length_in_slots < 2) {
+            cerr << "Error: Window length for sliding window counter must be "
+                    "at least two\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    void increment_count(T obj) {
+        obj_counter.increment_count(obj, head_slot);
+    }
+
+    void increment_count(T obj, unsigned long increment) {
+        obj_counter.increment_count(obj, head_slot, increment);
+    }
+
+    unordered_map<T, unsigned long> get_counts_then_advance_window() {
+        const auto counts = obj_counter.get_counts();
+        obj_counter.wipe_zeros();
+        obj_counter.wipe_slot(tail_slot);
+        advance_head();
+        return counts;
+    }
+};
+
+template<typename T>
+class CircularFifoBuffer {
+    vector<T> buffer;
+    size_t    head {0};
+
+public:
+    CircularFifoBuffer(size_t size) : buffer(size) {
+        if (size == 0) {
+            cerr << "Error initializing circular buffer: size must be "
+                    "positive\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    size_t max_size() const {
+        return buffer.size();
+    }
+
+    void add(const T &element) {
+        buffer[head] = element;
+        head         = (head + 1) % buffer.size();
+    }
+
+    const T &get() const {
+        return buffer[head];
+    }
+
+    T &get() {
+        return buffer[head];
+    }
+};
+
+inline uint64_t current_time_msecs() __attribute__((always_inline));
+inline uint64_t current_time_msecs() {
+    struct timespec t;
+    clock_gettime(CLOCK_REALTIME, &t);
+    return (t.tv_sec) * 1000UL + (t.tv_nsec / 1000000UL);
+}
+
+class NthLastModifiedTimeTracker {
+    static constexpr auto             millis_in_sec = 1000u;
+    CircularFifoBuffer<unsigned long> last_modified_times_millis;
+
+    void init_last_modified_times_millis() {
+        const auto now_cached = current_time_msecs();
+        for (size_t i {0}; i < last_modified_times_millis.max_size(); ++i) {
+            last_modified_times_millis.add(now_cached);
+        }
+    }
+
+    void update_last_modified_time() {
+        last_modified_times_millis.add(current_time_msecs());
+    }
+
+public:
+    NthLastModifiedTimeTracker(size_t num_times_to_track)
+        : last_modified_times_millis {num_times_to_track} {
+        if (num_times_to_track < 1) {
+            cerr << "Error: num_times_to_track must be positive\n";
+            exit(EXIT_FAILURE);
+        }
+        init_last_modified_times_millis();
+    }
+
+    unsigned seconds_since_oldest_modification() {
+        const auto modified_time_millis = last_modified_times_millis.get();
+        return (unsigned) (current_time_msecs() - modified_time_millis);
+    }
+
+    void mark_as_modified() {
+        update_last_modified_time();
+    }
+};
 
 /*
  * Return difference between a and b, accounting for unsigned arithmetic
@@ -251,13 +452,6 @@ static inline vector<string_view> string_split(const string_view &s,
         word_begin = find_if_not(word_end, s.end(), is_delim);
     }
     return words;
-}
-
-inline uint64_t current_time_msecs() __attribute__((always_inline));
-inline uint64_t current_time_msecs() {
-    struct timespec t;
-    clock_gettime(CLOCK_REALTIME, &t);
-    return (t.tv_sec) * 1000UL + (t.tv_nsec / 1000000UL);
 }
 
 static inline vector<string> get_tweets_from_file(const char *filename) {
@@ -553,198 +747,18 @@ public:
     }
 };
 
-template<typename T>
-class SlotBasedCounter {
-    unordered_map<T, vector<unsigned long>> counts_map;
-    size_t                                  num_slots;
-
-    unsigned long compute_total_count(T obj) {
-        if (counts_map.find(obj) == counts_map.end()) {
-            return 0;
-        }
-
-        const auto &  curr_obj_counts = counts_map[obj];
-        unsigned long total {0};
-        for (const auto count : curr_obj_counts) {
-            total += count;
-        }
-        return total;
-    }
-
-    void reset_slot_count_to_zero(T obj, unsigned slot) {
-        counts_map[obj][slot] = 0;
-    }
-
-    bool should_be_removed_from_counter(T obj) {
-        return compute_total_count(obj) == 0;
-    }
-
+class TopicExtractorFunctor {
 public:
-    SlotBasedCounter(size_t num_slots) : num_slots {num_slots} {}
-
-    void increment_count(T obj, size_t slot, unsigned long increment) {
-        assert(slot < num_slots);
-
-        // XXX: this should work as expected, but double check!
-        if (counts_map.find(obj) == counts_map.end()) {
-            counts_map[obj].reserve(num_slots);
-        }
-        counts_map[obj][slot] += increment;
-    }
-
-    void increment_count(T obj, size_t slot) {
-        increment_count(obj, slot, 1);
-    }
-
-    unsigned long get_count(T obj, size_t slot) {
-        assert(slot < num_slots);
-
-        if (counts_map.find(obj) == counts_map.end()) {
-            return 0;
-        }
-        return counts_map[obj][slot];
-    }
-
-    unordered_map<T, unsigned long> get_counts() {
-        unordered_map<T, unsigned long> result;
-
-        for (const auto &kv : counts_map) {
-            result[kv.first] = compute_total_count(kv.first);
-        }
-        return result;
-    }
-
-    void wipe_slot(size_t slot) {
-        assert(slot < num_slots);
-
-        for (const auto &kv : counts_map) {
-            reset_slot_count_to_zero(kv.first, slot);
-        }
-    }
-
-    void wipe_zeros() {
-        vector<T> objs_to_be_removed;
-        for (const auto &kv : counts_map) {
-            if (should_be_removed_from_counter(kv.first)) {
-                objs_to_be_removed.push_back(kv.first);
+    void operator()(const Tweet &tweet, Shipper<Topic> &shipper) {
+        if (!tweet.text.empty()) {
+            const auto words = string_split(tweet.text, " \n\t");
+            for (const auto &word : words) {
+                assert(!word.empty());
+                if (word[0] == '#') {
+                    shipper.push({tweet.metadata, string {word}});
+                }
             }
         }
-        for (const auto &obj : objs_to_be_removed) {
-            counts_map.erase(obj);
-        }
-    }
-};
-
-template<typename T>
-class SlidingWindowCounter {
-    SlotBasedCounter<T> obj_counter;
-    size_t              head_slot;
-    size_t              tail_slot;
-    size_t              windw_length_in_slots;
-
-    size_t slot_after(size_t slot) {
-        assert(slot < windw_length_in_slots);
-        return (slot + 1) % windw_length_in_slots;
-    }
-
-    void advance_head() {
-        head_slot = tail_slot;
-        tail_slot = slot_after(tail_slot);
-    }
-
-public:
-    SlidingWindowCounter(size_t windw_length_in_slots)
-        : obj_counter {windw_length_in_slots}, head_slot {0},
-          tail_slot {slot_after(head_slot)}, windw_length_in_slots {
-                                                 windw_length_in_slots} {
-        if (windw_length_in_slots < 2) {
-            cerr << "Error: Window length for sliding window counter must be "
-                    "at least two\n";
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    void increment_count(T obj) {
-        obj_counter.increment_count(obj, head_slot);
-    }
-
-    void increment_count(T obj, unsigned long increment) {
-        obj_counter.increment_count(obj, head_slot, increment);
-    }
-
-    unordered_map<T, unsigned long> get_counts_then_advance_window() {
-        const auto counts = obj_counter.get_counts();
-        obj_counter.wipe_zeros();
-        obj_counter.wipe_slot(tail_slot);
-        advance_head();
-        return counts;
-    }
-};
-
-template<typename T>
-class CircularFifoBuffer {
-    vector<T> buffer;
-    size_t    head {0};
-
-public:
-    CircularFifoBuffer(size_t size) : buffer(size) {
-        if (size == 0) {
-            cerr << "Error initializing circular buffer: size must be "
-                    "positive\n";
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    size_t max_size() const {
-        return buffer.size();
-    }
-
-    void add(const T &element) {
-        buffer[head] = element;
-        head         = (head + 1) % buffer.size();
-    }
-
-    const T &get() const {
-        return buffer[head];
-    }
-
-    T &get() {
-        return buffer[head];
-    }
-};
-
-class NthLastModifiedTimeTracker {
-    static constexpr auto             millis_in_sec = 1000u;
-    CircularFifoBuffer<unsigned long> last_modified_times_millis;
-
-    void init_last_modified_times_millis() {
-        const auto now_cached = current_time_msecs();
-        for (size_t i {0}; i < last_modified_times_millis.max_size(); ++i) {
-            last_modified_times_millis.add(now_cached);
-        }
-    }
-
-    void update_last_modified_time() {
-        last_modified_times_millis.add(current_time_msecs());
-    }
-
-public:
-    NthLastModifiedTimeTracker(size_t num_times_to_track)
-        : last_modified_times_millis {num_times_to_track} {
-        if (num_times_to_track < 1) {
-            cerr << "Error: num_times_to_track must be positive\n";
-            exit(EXIT_FAILURE);
-        }
-        init_last_modified_times_millis();
-    }
-
-    unsigned seconds_since_oldest_modification() {
-        const auto modified_time_millis = last_modified_times_millis.get();
-        return (unsigned) (current_time_msecs() - modified_time_millis);
-    }
-
-    void mark_as_modified() {
-        update_last_modified_time();
     }
 };
 
@@ -921,6 +935,14 @@ static inline PipeGraph &build_graph(const Parameters &parameters,
                       .withOutputBatchSize(parameters.batch_size)
                       .build();
 
+    TopicExtractorFunctor topic_extractor_functor;
+    auto                  topic_extractor_node =
+        FlatMap_Builder {topic_extractor_functor}
+            .withParallelism(parameters.topic_extractor_parallelism)
+            .withName("topic extractor")
+            .withOutputBatchSize(parameters.batch_size)
+            .build();
+
     RollingCounterFunctor rolling_counter_functor;
     auto                  rolling_counter_node =
         FlatMap_Builder {rolling_counter_functor}
@@ -956,12 +978,14 @@ static inline PipeGraph &build_graph(const Parameters &parameters,
 
     if (parameters.use_chaining) {
         graph.add_source(source)
+            .chain(topic_extractor_node)
             .chain(rolling_counter_node)
             .chain(intermediate_ranker_node)
             .chain(total_ranker_node)
             .chain_sink(sink);
     } else {
         graph.add_source(source)
+            .add(topic_extractor_node)
             .add(rolling_counter_node)
             .add(intermediate_ranker_node)
             .add(total_ranker_node)
