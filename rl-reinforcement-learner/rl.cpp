e288ad1f8516f750140fcf6f7bbbf0ba2b33f67d
cosimone64 <cosimo.agati@disroot.org>
2022-04-19 11:16:47 +0200

    Add Parameters struct

--- rl-reinforcement-learner/rl.cpp
+++ rl-reinforcement-learner/rl.cpp
@@ -6,6 +6,17 @@ using namespace std;
 using namespace wf;
 
 struct TupleMetadata {
+struct Parameters {
+    unsigned ctr_generator_parallelism {1};
+    unsigned reward_source_parallelism {1};
+    unsigned reinforcement_learner_parallelism {1};
+    unsigned sink_parallelism {1};
+    unsigned batch_size {0};
+    unsigned duration {60};
+    unsigned tuple_rate {1000};
+    unsigned sampling_rate {100};
+    bool     use_chaining {false};
+};
     unsigned long id;
     unsigned long timestamp;
 };
