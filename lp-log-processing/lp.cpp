91d76d67b230c45e3b05a49944f66851165bef1b
cosimone64 <cosimo.agati@disroot.org>
2022-04-28 12:45:38 +0200

    Remove outdated TODO notice

--- lp-log-processing/lp.cpp
+++ lp-log-processing/lp.cpp
@@ -305,7 +305,6 @@ static inline optional<SourceTuple> build_source_tuple(const string &line) {
     if (tokens.size() != 9) {
         return {};
     }
-    // TODO: Obtain minutes!
     SourceTuple tuple;
     tuple.ip            = tokens[1];
     tuple.log_timestamp = tokens[4];
