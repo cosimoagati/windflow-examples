426723c536ce50930145fe7e03e07077bf5f416b
cosimone64 <cosimo.agati@disroot.org>
2022-05-09 19:19:08 +0200

    Add basic LP test script

---
+++ lp-log-processing/lp-test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+duration=120
+datecmd="date +%Y-%m-%d-%H-%M"
+
+echo "Test started on $(date)"
+
+for chaining in false true; do
+    for i in $(seq 1 22); do
+        outputdir="testresults-$($datecmd)"
+        mkdir $outputdir
+        ./lp --duration="$duration" --parallelism="$i,$i,$i" \
+             --chaining="$chaining" \
+             --outputdir="$outputdir" >> "$outputdir/output.txt"
+    done
+done
+
+echo "Test completed on $(date)"
+
