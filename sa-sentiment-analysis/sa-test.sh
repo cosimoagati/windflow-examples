8fccbf1af97cfda038ed1f808b8ad9c97c679532
cosimone64 <cosimo.agati@disroot.org>
2022-05-09 19:18:54 +0200

    Send output of command in tests to a file

--- sa-sentiment-analysis/sa-test.sh
+++ sa-sentiment-analysis/sa-test.sh
@@ -7,8 +7,11 @@ echo "Test started on $(date)"
 
 for chaining in false true; do
     for i in $(seq 1 22); do
+        outputdir="testresults-$($datecmd)"
+        mkdir $outputdir
         ./sa --duration="$duration" --parallelism="$i,$i,$i" \
-             --chaining="$chaining"  --outputdir="testresults-$($datecmd)"
+             --chaining="$chaining" \
+             --outputdir="$outputdir" >> "$outputdir/output.txt"
     done
 done
 
