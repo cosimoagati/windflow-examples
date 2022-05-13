4c3469680ae42bcb93fbf30a670a5e2a8bc50418
cosimone64 <cosimo.agati@disroot.org>
2022-05-13 19:40:53 +0200

    Print out individual executions in test script

--- sa-sentiment-analysis/sa-test.sh
+++ sa-sentiment-analysis/sa-test.sh
@@ -10,10 +10,12 @@ for batching in 0 10 100 1000 1000; do
         for i in $(seq 1 22); do
             outputdir="testresults-$($datecmd)"
             mkdir $outputdir
+            set -x
             ./sa --duration="$duration" --parallelism="$i,$i,$i" \
                  --batch="$batching,$batching" \
                  --chaining="$chaining" \
                  --outputdir="$outputdir" >> "$outputdir/output.txt"
+            set +x
         done
     done
 done
