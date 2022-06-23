7549d3e8642dc3f6cb04c1dc285490f7e549b53c
cosimone64 <cosimo.agati@disroot.org>
2022-06-23 15:30:47 +0200

    Simplify test scripts
    Just dump all tests in a single directory, it’s easier to deal with
    this way.

--- mo-machine-outlier/mo-test.sh
+++ mo-machine-outlier/mo-test.sh
@@ -2,20 +2,21 @@
 
 duration=120
 datecmd="date +%Y-%m-%d-%H-%M"
+outputdir="testresults-$($datecmd)"
 
 cd $(dirname $0)
 echo "Test started on $(date)"
+mkdir -p "$outputdir"
 
 for batching in 0 10 100 1000 10000; do
     for chaining in false true; do
         for i in $(seq 1 22); do
-            outputdir="testresults-$($datecmd)"
-            mkdir -p $outputdir
             set -x
             ./mo --duration="$duration" --parallelism="$i,$i,$i,$i,$i" \
                  --batch="$batching,$batching,$batching,$batching" \
                  --chaining="$chaining" \
-                 --outputdir="$outputdir" >> "$outputdir/output.txt"
+                 --outputdir="$outputdir" \
+                 >> "$outputdir/output-$($datecmd).txt"
             set +x
         done
     done
