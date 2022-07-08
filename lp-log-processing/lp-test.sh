9850a8f690f9bd3ad10f54a84779605f958ec704
cosimone64 <cosimo.agati@disroot.org>
2022-07-08 10:54:18 +0200

    Use nproc / 6 parallelism degree per node with chaining as well

--- lp-log-processing/lp-test.sh
+++ lp-log-processing/lp-test.sh
@@ -9,31 +9,20 @@ echo "Test started on $(date)"
 mkdir -p "$outputdir"
 
 for rate in 0; do
-    for batching in 0 1 2 4 8 16 32 64 128; do
-        for pardeg in $(seq 1 $(expr $(nproc) / 6)); do
-            set -x
-            ./lp --duration="$duration" \
-                 --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
-                 --batch="$batching,$batching,$batching,$batching,$batching" \
-                 --chaining=false \
-                 --sampling=0 \
-                 --rate="$rate" \
-                 --outputdir="$outputdir" \
-                 >> "$outputdir/output-$($datecmd).txt"
-            set +x
-        done
-
-        for pardeg in $(seq 1 $(nproc)); do
-            set -x
-            ./lp --duration="$duration" \
-                 --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
-                 --batch="$batching,$batching,$batching,$batching,$batching" \
-                 --chaining=true \
-                 --sampling=0 \
-                 --rate="$rate" \
-                 --outputdir="$outputdir" \
-                 >> "$outputdir/output-$($datecmd).txt"
-            set +x
+    for chaining in false true; do
+        for batching in 0 1 2 4 8 16 32 64 128; do
+            for pardeg in $(seq 1 $(expr $(nproc) / 6)); do
+                set -x
+                ./lp --duration="$duration" \
+                     --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
+                     --batch="$batching,$batching,$batching,$batching,$batching" \
+                     --chaining="$chaining" \
+                     --sampling=0 \
+                     --rate="$rate" \
+                     --outputdir="$outputdir" \
+                     >> "$outputdir/output-$($datecmd).txt"
+                set +x
+            done
         done
     done
 done
