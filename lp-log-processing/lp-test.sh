f5f68aec15580b22e7bb51a1b69d0f03364160ba
cosimone64 <cosimo.agati@disroot.org>
2022-07-08 10:53:15 +0200

    Do not use batching value 6, it’s not a power of two

--- lp-log-processing/lp-test.sh
+++ lp-log-processing/lp-test.sh
@@ -9,7 +9,7 @@ echo "Test started on $(date)"
 mkdir -p "$outputdir"
 
 for rate in 0; do
-    for batching in 0 1 2 4 6 8 16 32 64 128; do
+    for batching in 0 1 2 4 8 16 32 64 128; do
         for pardeg in $(seq 1 $(expr $(nproc) / 6)); do
             set -x
             ./lp --duration="$duration" \
