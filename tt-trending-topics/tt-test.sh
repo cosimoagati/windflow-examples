075e4313ec1518b455301294627e61d3494d6e51
cosimone64 <cosimo.agati@disroot.org>
2022-07-10 22:03:20 +0200

    Change parallelism degree limits

--- tt-trending-topics/tt-test.sh
+++ tt-trending-topics/tt-test.sh
@@ -3,6 +3,7 @@
 duration=120
 datecmd="date +%Y-%m-%d-%H-%M"
 outputdir="testresults-$($datecmd)"
+nproc=$(nproc)
 
 cd $(dirname "$0")
 echo "Test started on $(date)"
@@ -10,39 +11,49 @@ mkdir -p "$outputdir"
 
 for rate in 0; do
     for batching in 0 1 2 4 8 16 32 64 128; do
-        for pardeg in $(seq 1 $(expr $(nproc) / 6)); do
+        for pardeg in $(seq 1 $((($nproc / 6) - 3))); do
             set -x
             ./tt-timer-functors \
                 --duration="$duration" \
                 --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
                 --batch="$batching,$batching,$batching,$batching,$batching" \
-                --chaining="$chaining" \
+                --chaining=false \
                 --rate=false \
                 --outputdir="$outputdir" >> "$outputdir/output-$($datecmd).txt"
-            ./tt-timer-threads \
+            set +x
+        done
+
+        for pardeg in $(seq 1 $((($nproc / 4) - 3))); do
+            set -x
+            ./tt-timer-functors \
                 --duration="$duration" \
                 --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
                 --batch="$batching,$batching,$batching,$batching,$batching" \
-                --chaining="$chaining" \
+                --chaining=true \
                 --rate=false \
                 --outputdir="$outputdir" >> "$outputdir/output-$($datecmd).txt"
             set +x
         done
         
-        for pardeg in $(seq 1 $(nproc)); do
+        for pardeg in $(seq 1 $(($nproc / 6))); do
             set -x
-            ./tt-timer-functors \
-                --duration="$duration" \
-                --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
-                --batch="$batching,$batching,$batching,$batching,$batching" \
-                --chaining="$chaining" \
-                --rate=true \
-                --outputdir="$outputdir" >> "$outputdir/output-$($datecmd).txt"
             ./tt-timer-threads \
                 --duration="$duration" \
                 --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
                 --batch="$batching,$batching,$batching,$batching,$batching" \
-                --chaining="$chaining" \
+                --chaining=false \
+                --rate=true \
+                --outputdir="$outputdir" >> "$outputdir/output-$($datecmd).txt"
+            set +x
+        done
+        
+        for pardeg in $(seq $(($nproc / 3))); do
+            set +x
+            ./tt-timer-threads \
+                --duration="$duration" \
+                --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
+                --batch="$batching,$batching,$batching,$batching,$batching" \
+                --chaining=true \
                 --rate=true \
                 --outputdir="$outputdir" >> "$outputdir/output-$($datecmd).txt"
             set +x
