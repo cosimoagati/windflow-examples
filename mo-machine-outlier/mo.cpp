177922447c837835840bff2f5e927aa398d27299
cosimone64 <cosimo.agati@disroot.org>
2022-07-16 17:26:32 +0200

    Use an additional tabstop to align output

--- mo-machine-outlier/mo.cpp
+++ mo-machine-outlier/mo.cpp
@@ -397,7 +397,7 @@ static inline void print_initial_parameters(const Parameters &parameters) {
 
     cout << "Chaining:\t" << (parameters.use_chaining ? "enabled" : "disabled")
          << '\n'
-         << "Anomaly Scorer variant:\t" << parameters.anomaly_scorer_type
+         << "Anomaly Scorer variant:\t\t" << parameters.anomaly_scorer_type
          << '\n'
          << "Alert Triggerer variant:\t" << parameters.alert_triggerer_type
          << '\n';
