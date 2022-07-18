99a2bdb35529a717a1e57b4cf5520701ef43a4c4
cosimone64 <cosimo.agati@disroot.org>
2022-07-18 21:40:24 +0200

    Loop over execution modes (deterministic and default)

--- mo-machine-outlier/mo-test.sh
+++ mo-machine-outlier/mo-test.sh
@@ -11,34 +11,38 @@ mkdir -p "$outputdir"
 
 set -x
 for rate in 0; do
-    for anomaly_scorer in data-stream sliding-window; do
-        for alert_triggerer in top-k default; do
-            current_outputdir="$outputdir/$anomaly_scorer-$alert_triggerer"
-            mkdir -p "$current_outputdir"
-            
-            for batching in 0 1 2 4 8 16 32 64 128; do
-                for pardeg in $(seq 1 $(($nproc / 5))); do
-                    ./mo --duration="$duration" \
-                         --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
-                         --batch="$batching,$batching,$batching,$batching" \
-                         --chaining=false \
-                         --rate="$rate" \
-                         --anomalyscorer="$anomaly_scorer" \
-                         --alerttriggerer="$alert_triggerer" \
-                         --outputdir="$current_outputdir" \
-                         >> "$current_outputdir/output-$($datecmd).txt"
-                done
+    for execmode in deterministic default; do
+        for anomaly_scorer in data-stream sliding-window; do
+            for alert_triggerer in top-k default; do
+                current_outputdir="$outputdir/$anomaly_scorer-$alert_triggerer"
+                mkdir -p "$current_outputdir"
+                
+                for batching in 0 1 2 4 8 16 32 64 128; do
+                    for pardeg in $(seq 1 $(($nproc / 5))); do
+                        ./mo --duration="$duration" \
+                             --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
+                             --batch="$batching,$batching,$batching,$batching" \
+                             --chaining=false \
+                             --rate="$rate" \
+                             --execmode="$execmode" \
+                             --anomalyscorer="$anomaly_scorer" \
+                             --alerttriggerer="$alert_triggerer" \
+                             --outputdir="$current_outputdir" \
+                             >> "$current_outputdir/output-$($datecmd).txt"
+                    done
 
-                for pardeg in $(seq 1 $(($nproc / 2))); do
-                    ./mo --duration="$duration" \
-                         --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
-                         --batch="$batching,$batching,$batching,$batching" \
-                         --chaining=true \
-                         --rate="$rate" \
-                         --anomalyscorer="$anomaly_scorer" \
-                         --alerttriggerer="$alert_triggerer" \
-                         --outputdir="$current_outputdir" \
-                         >> "$current_outputdir/output-$($datecmd).txt"
+                    for pardeg in $(seq 1 $(($nproc / 2))); do
+                        ./mo --duration="$duration" \
+                             --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
+                             --batch="$batching,$batching,$batching,$batching" \
+                             --chaining=true \
+                             --rate="$rate" \
+                             --execmode="$execmode" \
+                             --anomalyscorer="$anomaly_scorer" \
+                             --alerttriggerer="$alert_triggerer" \
+                             --outputdir="$current_outputdir" \
+                             >> "$current_outputdir/output-$($datecmd).txt"
+                    done
                 done
             done
         done
