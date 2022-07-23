b5a68baf13409dd41173d1c077e0e9133db748a8
cosimone64 <cosimo.agati@disroot.org>
2022-07-23 19:13:02 +0200

    Use an additional loop for the --timernodes option

--- tt-trending-topics/tt-test.sh
+++ tt-trending-topics/tt-test.sh
@@ -16,15 +16,17 @@ for rate in 0; do
     for freq in 2 4 6 8 10; do
         for batching in 0 1 2 4 8 16 32 64 128; do
             for pardeg in $(seq 1 $((($nproc - $timerthreads) / 6))); do
-                ./tt --duration="$duration" \
-                     --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
-                     --batch="$batching,$batching,$batching,$batching,$batching" \
-                     --chaining=false \
-                     --frequency="$freq,$freq,$freq" \
-                     --rate="$rate" \
-                     --timernodes=true \
-                     --outputdir="$outputdir/tt-functors" \
-                     >> "$outputdir/output-$($datecmd).txt"
+                for usetimernodes in false true; do
+                    ./tt --duration="$duration" \
+                         --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
+                         --batch="$batching,$batching,$batching,$batching,$batching" \
+                         --chaining=false \
+                         --frequency="$freq,$freq,$freq" \
+                         --rate="$rate" \
+                         --timernodes="$usetimernodes" \
+                         --outputdir="$outputdir/tt-functors" \
+                         >> "$outputdir/output-$($datecmd).txt"
+                done
             done
 
             for pardeg in $(seq 1 $((($nproc - $timerthreads) / 4))); do
@@ -36,18 +38,6 @@ for rate in 0; do
                      --rate="$rate" \
                      --timernodes=true \
                      --outputdir="$outputdir/tt-functors" \
-                     >> "$outputdir/output-$($datecmd).txt"
-            done
-            
-            for pardeg in $(seq 1 $((($nproc - $timerthreads) / 6))); do
-                ./tt --duration="$duration" \
-                     --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
-                     --batch="$batching,$batching,$batching,$batching,$batching" \
-                     --chaining=false \
-                     --frequency="$freq,$freq,$freq" \
-                     --rate="$rate" \
-                     --timernodes=false \
-                     --outputdir="$outputdir/tt-threads" \
                      >> "$outputdir/output-$($datecmd).txt"
             done
             
