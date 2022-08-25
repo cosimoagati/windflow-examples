99845d4dd8122a9cc9c80bf3796d443f823d2834
cosimone64 <cosimo.agati@disroot.org>
2022-08-25 23:12:39 +0200

    Do not subtract periodic nodes when not using separate sources

--- tt-trending-topics/tt-test.sh
+++ tt-trending-topics/tt-test.sh
@@ -19,17 +19,27 @@ for rate in 0; do
     for freq in $frequencies; do
         for batching in 0 1 2 4 8 16 32 64 128; do
             for pardeg in $(seq 1 $((($nproc - $timerthreads) / 6))); do
-                for usetimernodes in false true; do
-                    ./tt --duration=$duration \
-                         --parallelism=$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg \
-                         --batch=$batching,$batching,$batching,$batching,$batching \
-                         --chaining=false \
-                         --frequency=$freq,$freq,$freq \
-                         --rate=$rate \
-                         --timernodes=$usetimernodes \
-                         --outputdir="$outputdir" \
-                         >> "$outputdir/output-$($datecmd).txt"
-                done
+                ./tt --duration=$duration \
+                     --parallelism=$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg \
+                     --batch=$batching,$batching,$batching,$batching,$batching \
+                     --chaining=false \
+                     --frequency=$freq,$freq,$freq \
+                     --rate=$rate \
+                     --timernodes=true \
+                     --outputdir="$outputdir" \
+                     >> "$outputdir/output-$($datecmd).txt"
+            done
+
+            for pardeg in $(seq 1 $(($nproc / 6))); do
+                ./tt --duration=$duration \
+                     --parallelism=$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg \
+                     --batch=$batching,$batching,$batching,$batching,$batching \
+                     --chaining=false \
+                     --frequency=$freq,$freq,$freq \
+                     --rate=$rate \
+                     --timernodes=false \
+                     --outputdir="$outputdir" \
+                     >> "$outputdir/output-$($datecmd).txt"
             done
 
             for pardeg in $(seq 1 $((($nproc - $timerthreads) / 4))); do
@@ -44,7 +54,7 @@ for rate in 0; do
                      >> "$outputdir/output-$($datecmd).txt"
             done
             
-            for pardeg in $(seq $((($nproc - $timerthreads) / 3))); do
+            for pardeg in $(seq $(($nproc / 3))); do
                 ./tt --duration=$duration \
                      --parallelism=$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg \
                      --batch=$batching,$batching,$batching,$batching,$batching \
