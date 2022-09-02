1f5ecd7b64df6bbb8c364b94e01636da74bbd8f7
cosimone64 <cosimo.agati@disroot.org>
2022-09-02 12:13:09 +0200

    Don’t loop over chaining values
    Chaining does not change the topology in any way for Log Processing,
    hence it’s pointless to do tests for chaining as well.

--- lp-log-processing/lp-test.sh
+++ lp-log-processing/lp-test.sh
@@ -12,18 +12,16 @@ mkdir -p "$outputdir"
 set -x
 make -j$nproc -B
 for rate in 0; do
-    for chaining in false true; do
-        for batching in 0 1 2 4 8 16 32 64 128; do
-            for pardeg in $(seq 1 $(($nproc / 6))); do
-                ./lp --duration="$duration" \
-                     --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
-                     --batch="$batching,$batching,$batching,$batching,$batching" \
-                     --chaining="$chaining" \
-                     --sampling=0 \
-                     --rate="$rate" \
-                     --outputdir="$outputdir" \
-                     >> "$outputdir/output-$($datecmd).txt"
-            done
+    for batching in 0 1 2 4 8 16 32 64 128; do
+        for pardeg in $(seq 1 $(($nproc / 6))); do
+            ./lp --duration="$duration" \
+                 --parallelism="$pardeg,$pardeg,$pardeg,$pardeg,$pardeg,$pardeg" \
+                 --batch="$batching,$batching,$batching,$batching,$batching" \
+                 --chaining=false \
+                 --sampling=0 \
+                 --rate="$rate" \
+                 --outputdir="$outputdir" \
+                 >> "$outputdir/output-$($datecmd).txt"
         done
     done
 done
