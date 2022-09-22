683d15ff2290d6a0703116ce20728b685deb2bc1
cosimone64 <cosimo.agati@disroot.org>
2022-09-23 01:39:52 +0200

    Propagate timestamps in a better way

--- mo-machine-outlier/mo.cpp
+++ mo-machine-outlier/mo.cpp
@@ -656,8 +656,8 @@ void process_observations(const SourceTuple &tuple, RuntimeContext &context) {
                 data.scorer.get_scores(data.observation_list);
             const unsigned long next_ordering_timestamp =
                 data.execution_mode == Execution_Mode_t::DEFAULT
-                    ? context.getLastWatermark()
-                    : tuple.ordering_timestamp;
+                    ? tuple.ordering_timestamp
+                    : data.previous_ordering_timestamp;
 
             for (const auto &package : score_package_list) {
                 ObservationResultTuple result {
@@ -743,7 +743,6 @@ public:
 
             while (!tuple_queue.empty()
                    && tuple_queue.top().ordering_timestamp <= watermark) {
-                assert(tuple_queue.top().ordering_timestamp == watermark);
                 process_observations<Scorer>(tuple_queue.top(), context);
                 tuple_queue.pop();
             }
@@ -782,8 +781,8 @@ void process_data_stream_anomalies(const ObservationResultTuple &tuple,
     if (tuple.ordering_timestamp > data.previous_ordering_timestamp) {
         const unsigned long next_ordering_timestamp =
             data.execution_mode == Execution_Mode_t::DEFAULT
-                ? context.getLastWatermark()
-                : tuple.ordering_timestamp;
+                ? tuple.ordering_timestamp
+                : data.previous_ordering_timestamp;
 
         for (auto &entry : data.stream_profile_map) {
             auto &stream_profile = entry.second;
@@ -881,7 +880,6 @@ public:
 
             while (!tuple_queue.empty()
                    && tuple_queue.top().ordering_timestamp <= watermark) {
-                assert(tuple_queue.top().ordering_timestamp == watermark);
                 process_data_stream_anomalies<T>(tuple_queue.top(), context);
                 tuple_queue.pop();
             }
@@ -928,10 +926,7 @@ void process_sliding_window_anomalies(const ObservationResultTuple &tuple,
              << ", individual score: " << tuple.score << '\n';
     }
 #endif
-    const unsigned long next_ordering_timestamp =
-        data.execution_mode == Execution_Mode_t::DEFAULT
-            ? context.getLastWatermark()
-            : tuple.ordering_timestamp;
+    const unsigned long next_ordering_timestamp = tuple.ordering_timestamp;
 
     data.shipper->push({tuple.id, score_sum, next_ordering_timestamp,
                         tuple.parent_execution_timestamp, tuple.observation,
